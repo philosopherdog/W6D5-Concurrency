573b7c0edd71f4ee5293a45a071b3005713f018b
steve <steven.thompson.ca@gmail.com>
2016-12-06 20:42:25 -0500

    Adds test target

---
+++ SwiftBlockingExampleTests/SwiftBlockingExampleTests.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftBlockingExampleTests.swift
+//  SwiftBlockingExampleTests
+//
+//  Created by steve on 2016-12-06.
+//  Copyright © 2016 steve. All rights reserved.
+//
+
+import XCTest
+
+class SwiftBlockingExampleTests: XCTestCase {
+  
+  override func setUp() {
+    super.setUp()
+  }
+  
+  override func tearDown() {
+    super.tearDown()
+  }
+  
+}
